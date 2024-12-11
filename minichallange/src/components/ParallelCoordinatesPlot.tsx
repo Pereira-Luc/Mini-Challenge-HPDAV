b2b3ac9d0dcfb73551ddf0faaf369c1beef3a004
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2024-12-11 17:58:55 +0100

    add state management for firewall data and implement data fetching in ParallelCoordinatesPlot component

--- minichallange/src/components/ParallelCoordinatesPlot.tsx
+++ minichallange/src/components/ParallelCoordinatesPlot.tsx
@@ -1,23 +1,38 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getFirewallDataByDateTimeRange } from "../util/fetchers";
+import { FirewallData } from "../util/interface";
 
 const ParallelCoordinatesPlot = () => {
+    const [firewallData, setfirewallData] = useState<FirewallData[]>([]);
+
     useEffect(() => {
-        console.log("ParallelCoordinatesPlot component mounted");
-    }, []);
+        generateParallelCoordinatesPlot(firewallData, ["DateTime", "SourceIP", "DestinationIP"]);
+    }, [firewallData]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 console.log("Fetching data...");
-                const data = await getFirewallDataByDateTimeRange("2012-04-06T17:40:00", "2012-04-06T17:45:00");
-                console.log(data);
+                const data: FirewallData[] = await getFirewallDataByDateTimeRange("2012-04-06T17:40:00", "2012-04-06T17:45:00");
+                setfirewallData(data);
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         };
         fetchData();
     }, []);
+
+    const generateParallelCoordinatesPlot = (data: FirewallData[], dimensions: string[] = ["DateTime", "SourceIP", "DestinationIP"]) => { 
+        console.log("Generating Parallel Coordinates Plot...");
+        console.log("Data length:", data);
+        console.log("Dimensions:", dimensions);
+
+        if (data.length === 0 || !data) {
+            console.error("Data is empty");
+            return;
+        }
+
+    };
 
     return (
         <>
