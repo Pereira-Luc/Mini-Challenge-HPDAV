332cbac1837a2a214c24fb879f2f14b690ce3455
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2024-12-11 16:15:23 +0100

    Info about headers

--- minichallangeFlask/utils/dataProcessing.py
+++ minichallangeFlask/utils/dataProcessing.py
@@ -2,6 +2,46 @@ import os
 import glob
 import pandas as pd
 
+"""
+Heading Information for Firewall Data and Intrusion Detection Data
+
+Firewall Data:
+    | Field                | Description                  |
+    |----------------------|------------------------------|
+    | Date/time            | 06/Apr/2012 17:40:02         |
+    | Syslog priority      | Info (not only)              |
+    | Operation            | Built or Teardown            |
+    | Message code         | ASA-6-302015 (or others)     |
+    | Protocol             | TCP or UDP                   |
+    | Source IP            | IPV4 address                 |
+    | Destination IP       | IPV4 address                 |
+    | Source hostname      | (empty) maybe others         |
+    | Destination hostname | (empty) maybe others         |
+    | Source port          | some number                  |
+    | Destination port     | some number                  |
+    | Destination service  | http or other things         |
+    | Direction            | Inbound or Outbound          |
+    | Connections built    | 0 or 1                       |
+    | Connections torn down| 0 or 1                       |
+
+Intrusion Detection Data:
+    | Field          | Description             |
+    |----------------|-------------------------|
+    | time           | 4/6/2012 17:23          |
+    | sourceIP       | IPV4 address            |
+    | sourcePort     | some number             |
+    | destIP         | IPV4 address            |
+    | destPort       | some number             |
+    | classification | Text with information   |
+    | priority       | number                  |
+    | label          |                         |
+    | packet info    |                         |
+    | packet info cont'd|                      |
+    | xref           |                         |
+"""
+
+
+
 # Global variables to cache the data
 firewall_data_cache = None
 intrusion_detection_data_cache = None
